3537b981ffaa8ddda33a5ad7096e57bc9c63a31a
Joaquim <devjoaquim@gmail.com>
2026-07-23 01:06:32 +0200

    Reorganitzat el sistema d'inputs separats en una classe i recursos

---
+++ scripts/controls_jugador.gd
@@ -0,0 +1,13 @@
+class_name ControlsJugador
+extends Resource
+
+## Índex identificador del jugador
+@export var index_jugador: int = 0
+## Nom (al mapa d'entrades) de la tecla per moure's a l'esquerra
+@export var mou_esquerra: StringName = "p1_esquerra"
+## Nom (al mapa d'entrades) de la tecla per moure's a la dreta
+@export var mou_dreta: StringName = "p1_dreta"
+## Nom (al mapa d'entrades) de la tecla per moure's amunt
+@export var mou_amunt: StringName = "p1_amunt"
+## Nom (al mapa d'entrades) de la tecla per moure's avall
+@export var mou_avall: StringName = "p1_avall"
--- scripts/personatge.gd
+++ scripts/personatge.gd
@@ -6,30 +6,12 @@ extends CharacterBody2D
 @export var rapidesa_y: float
 ## Velocitat de salt
 @export var velocitat_salt: float
-## Identificador del personatge (pot ser només 0 - esquerra o 1 - dreta)
-@export_range(0,1) var id_personatge: int
-
-# Identifica moviment horitzontal amb la ID del personatge corresponent
-func input_horitzontal() -> float:
-	if id_personatge == 0:
-		return Input.get_axis("p1_esquerra", "p1_dreta")
-	else:
-		return Input.get_axis("p2_esquerra", "p2_dreta")
-
-# Identifica moviment vertical amb la ID del personatge corresponent
-func input_vertical() -> float:
-	if id_personatge == 0:
-		return Input.get_axis("p1_amunt", "p1_avall")
-	else:
-		return Input.get_axis("p2_amunt", "p2_avall")
-
-# Identifica si estem prement amunt amb la ID de personatge corresponent
-func amunt_premut() -> bool:
-	return (id_personatge == 0 and Input.is_action_just_pressed("p1_amunt")) or (id_personatge == 1 and Input.is_action_just_pressed("p2_amunt"))
+## Controls dels personatge
+@export var controls: ControlsJugador
 
 func _physics_process(delta: float) -> void:
 	# Input horitzontal, o frena
-	var direccio_x: float = input_horitzontal()
+	var direccio_x: float = Input.get_axis(controls.mou_esquerra, controls.mou_dreta)
 	if direccio_x:
 		velocity.x = direccio_x * rapidesa_x
 	else:
@@ -42,7 +24,7 @@ func _physics_process(delta: float) -> void:
 		if direccio_x == - normal.x:
 			# Estem "prement contra la paret". Escalem
 			escalant = true
-			var direccio_y: float = input_vertical()
+			var direccio_y: float = Input.get_axis(controls.mou_amunt, controls.mou_avall)
 			if direccio_y:
 				velocity.y = direccio_y * rapidesa_y
 			else:
@@ -57,7 +39,7 @@ func _physics_process(delta: float) -> void:
 		if not is_on_floor():
 			velocity += get_gravity() * delta
 		# Salt
-		if amunt_premut() and is_on_floor():
+		if Input.is_action_just_pressed(controls.mou_amunt) and is_on_floor():
 			velocity.y = velocitat_salt
 
 	move_and_slide()

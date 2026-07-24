93320e7ac0126ca3e0e8ab7929728f49e8afa6ab
Joaquim <devjoaquim@gmail.com>
2026-07-24 21:02:54 +0200

    Les bombes destrueixen terrenys ara

--- scripts/nivell.gd
+++ scripts/nivell.gd
@@ -14,13 +14,31 @@ func _on_personatge_2_objecte_alliberat(objecte: Item) -> void:
 	assigna_item(objecte)
 
 func resol_explosio(bomba: Explosiu) -> void:
-	#TODO:
-	# 2- Traduir-ho a coordenades del TileMap de roca
 	# 3- Eliminar les caselles que correspongui
 	var centre: Vector2 = to_local(bomba.get_global_position())
+	var casella_centre: Vector2i = capa_roca.local_to_map(centre)
 	var tipus_bomba: PropietatsExplosiu.TIPUS_EXPLOSIU = bomba.propietats.tipus_explosiu
 	var caselles_radi: int = bomba.propietats.radi_explosio
-	print("BUM! a " + str(centre))
+	# Bucle a través de les caselles dins del "diamant" de radi de l'explosió
+	for y in range(- caselles_radi, caselles_radi + 1):
+		for x in range( maxi(y - caselles_radi, - y - caselles_radi), mini(caselles_radi - y, y + caselles_radi) + 1):
+			var casella := Vector2i(x,y) + casella_centre
+			var data: TileData = capa_roca.get_cell_tile_data(casella)
+			if data != null:
+				# Hem picat roca
+				var terreny_casella: int = data.get_terrain()	# Si tot va bé, ens hauria de donar 0 per roca tova, 1 per roca dura, i 2 per roca impenetrable
+				#TODO: Hi havia una manera d'alterar el tilemap per terrenys?
+				match tipus_bomba:
+					PropietatsExplosiu.TIPUS_EXPLOSIU.Petit:
+						pass	#TODO quan hi hagi fòssils
+					PropietatsExplosiu.TIPUS_EXPLOSIU.Mitja:
+						if terreny_casella == 0:
+							# Roca tova
+							capa_roca.set_cell(casella, -1)	# Esborrem la casella del TileMap
+					PropietatsExplosiu.TIPUS_EXPLOSIU.Gran:
+						if terreny_casella != 2:
+							# Roca tova o dura
+							capa_roca.set_cell(casella, -1)	# Esborrem la casella del TileMap
 
 func _on_personatge_1_explosio_bomba(bomba: Explosiu) -> void:
 	resol_explosio(bomba)

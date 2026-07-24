ef8e7a1500c0a4cfbbecd40612bd0268cc151b9f
Joaquim <devjoaquim@gmail.com>
2026-07-24 19:46:08 +0200

    Feta la lògica de les explosions a Nivell

--- scripts/nivell.gd
+++ scripts/nivell.gd
@@ -1,6 +1,8 @@
 extends Node2D
 
 @onready var items: Node2D = $Items
+@onready var capa_roca: TileMapLayer = $Capes/CapaRoca
+
 
 func assigna_item(item: Item) -> void:
 	item.call_deferred("reparent", items)
@@ -13,10 +15,12 @@ func _on_personatge_2_objecte_alliberat(objecte: Item) -> void:
 
 func resol_explosio(bomba: Explosiu) -> void:
 	#TODO:
-	# 1- Aconseguir localització de la bomba, tipus i radi
 	# 2- Traduir-ho a coordenades del TileMap de roca
 	# 3- Eliminar les caselles que correspongui
-	print("BUM!")
+	var centre: Vector2 = to_local(bomba.get_global_position())
+	var tipus_bomba: PropietatsExplosiu.TIPUS_EXPLOSIU = bomba.propietats.tipus_explosiu
+	var caselles_radi: int = bomba.propietats.radi_explosio
+	print("BUM! a " + str(centre))
 
 func _on_personatge_1_explosio_bomba(bomba: Explosiu) -> void:
 	resol_explosio(bomba)

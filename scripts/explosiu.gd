572d6a4ec4ebb302b78c4677e3aa1519790c5499
Joaquim <devjoaquim@gmail.com>
2026-07-24 19:17:53 +0200

    Compte enrere per explosió

--- scripts/explosiu.gd
+++ scripts/explosiu.gd
@@ -4,11 +4,12 @@ extends Item
 ## Propietats de l'explosiu
 @export var propietats: PropietatsExplosiu
 
+var plantilla_etiqueta_compte_enrere = preload("uid://brbsl25c07q5f")
+
 # Indica quin és el darrer personatge que ha agafat aquest explosiu (0 a l'esquerra, 1 a la dreta)
 var darrer_propietari: int = -1
 var compte_enrere: int = 3
 
-signal temps_restant_bomba(temps: int, propietari: int)
 signal explosio_bomba(explosiu: Explosiu)
 
 func _ready() -> void:
@@ -23,10 +24,16 @@ func temporitzador_acabat() -> void:
 		print("Explosió!")
 	else:
 		# COMPTE ENRERE
-		#TODO
-		print(str(compte_enrere) + "!")
+		afegeix_etiqueta_compte_enrere()
 		compte_enrere -= 1
 		get_tree().create_timer(1, false).timeout.connect(temporitzador_acabat)
+
+func afegeix_etiqueta_compte_enrere() -> void:
+	var etiqueta_compte_enrere = plantilla_etiqueta_compte_enrere.instantiate()
+	etiqueta_compte_enrere.text = str(compte_enrere) + "!"
+	add_child(etiqueta_compte_enrere)
+	etiqueta_compte_enrere.position = Vector2(-100, -550)
+	get_tree().create_timer(0.8, false).timeout.connect(func (): etiqueta_compte_enrere.queue_free())
 
 #TODO: Quan s'acabi el temporitzador, enviar una senyal d'explosio a l'escena responsable
 #TODO: Quan s'acabi el temporitzador, fer l'animació d'explosió
